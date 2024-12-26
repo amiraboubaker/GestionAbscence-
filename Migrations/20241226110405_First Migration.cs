ce79ca6be17ee604cc9b94b0e8d36b5d88f7e5c3
Amira Boubaker <amiraboubakeresprims@gmail.com>
2024-12-26 12:06:05 +0100

    add Migrations/

---
+++ Migrations/20241226110405_First Migration.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionAbscence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Classe",
+                columns: table => new
+                {
+                    CodeClasse = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomClasse = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CodeGroupe = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CodeDepartement = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classe", x => x.CodeClasse);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departement",
+                columns: table => new
+                {
+                    CodeDepartement = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomDepartement = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departement", x => x.CodeDepartement);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enseignant",
+                columns: table => new
+                {
+                    CodeEnseignant = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateRecrutement = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Adresse = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tel = table.Column<int>(type: "int", nullable: false),
+                    CodeDepartement = table.Column<int>(type: "int", nullable: false),
+                    CodeGrade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enseignant", x => x.CodeEnseignant);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Etudiant",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateNaissance = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CodeClasse = table.Column<int>(type: "int", nullable: false),
+                    NumInscription = table.Column<int>(type: "int", nullable: false),
+                    Adresse = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tel = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Etudiant", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FicheAbsence",
+                columns: table => new
+                {
+                    CodeFicheAbsence = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateJour = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CodeMatiere = table.Column<int>(type: "int", nullable: false),
+                    CodeEnseignant = table.Column<int>(type: "int", nullable: false),
+                    CodeClasse = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FicheAbsence", x => x.CodeFicheAbsence);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FicheAbsenceSeance",
+                columns: table => new
+                {
+                    CodeFicheAbsence = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CodeSeance = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FicheAbsenceSeance", x => x.CodeFicheAbsence);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grade",
+                columns: table => new
+                {
+                    CodeGrade = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomGrade = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grade", x => x.CodeGrade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groupe",
+                columns: table => new
+                {
+                    CodeGroupe = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomGroupe = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groupe", x => x.CodeGroupe);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LigneFicheAbsence",
+                columns: table => new
+                {
+                    CodeFicheAbsence = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CodeSeance = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LigneFicheAbsence", x => x.CodeFicheAbsence);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matiere",
+                columns: table => new
+                {
+                    CodeMatiere = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomMatiere = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NbreHeureCoursParSemaine = table.Column<int>(type: "int", nullable: false),
+                    NbreHeureTDParSemaine = table.Column<int>(type: "int", nullable: false),
+                    NbreHeureTPParSemaine = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matiere", x => x.CodeMatiere);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seance",
+                columns: table => new
+                {
+                    CodeSeance = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomSeance = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HeureDebut = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HeureFin = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seance", x => x.CodeSeance);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Classe");
+
+            migrationBuilder.DropTable(
+                name: "Departement");
+
+            migrationBuilder.DropTable(
+                name: "Enseignant");
+
+            migrationBuilder.DropTable(
+                name: "Etudiant");
+
+            migrationBuilder.DropTable(
+                name: "FicheAbsence");
+
+            migrationBuilder.DropTable(
+                name: "FicheAbsenceSeance");
+
+            migrationBuilder.DropTable(
+                name: "Grade");
+
+            migrationBuilder.DropTable(
+                name: "Groupe");
+
+            migrationBuilder.DropTable(
+                name: "LigneFicheAbsence");
+
+            migrationBuilder.DropTable(
+                name: "Matiere");
+
+            migrationBuilder.DropTable(
+                name: "Seance");
+        }
+    }
+}
