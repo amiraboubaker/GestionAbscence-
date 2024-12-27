5e6ee39069bfe71e873ff636c92ecccfb356da29
Amira Boubaker <amiraboubakeresprims@gmail.com>
2024-12-27 14:32:12 +0100

    add EnseignantController

---
+++ Controllers/EnseignantController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestionAbscence.Controllers
+{
+    public class EnseignantController : Controller
+    {
+        // Action qui affiche la vue "Index.cshtml"
+        public IActionResult Index()
+        {
+            return View();  // Cela rendra la vue "Views/Enseignant/Index.cshtml"
+        }
+    }
+}
